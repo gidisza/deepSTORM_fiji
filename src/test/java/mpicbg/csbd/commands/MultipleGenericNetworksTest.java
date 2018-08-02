8e57761457124ae09a372d43c2f4dbc8d1064278
random <hallo@frauzufall.de>
2018-08-02 02:38:05 +0200

    adds test to reproduce error when running network on output again

--- src/test/java/mpicbg/csbd/commands/MultipleGenericNetworksTest.java
+++ src/test/java/mpicbg/csbd/commands/MultipleGenericNetworksTest.java
@@ -31,10 +31,10 @@ public class MultipleGenericNetworksTest extends CSBDeepTest {
 		String[] networks = {"/home/random/Development/imagej/project/CSBDeep/tests/generic_test2/denoise2D/model.zip",
 				"/home/random/Development/imagej/project/CSBDeep/tests/generic_test2/denoise3D/model.zip"};
 
-		for (int i = 0; i < 3; i++) {
+		Dataset input = createDataset(new FloatType(), new long[] { 10, 10, 10 }, new AxisType[] {
+				Axes.X, Axes.Y, Axes.Z });
 
-			final Dataset input = createDataset(new FloatType(), new long[] { 10, 10, 10 }, new AxisType[] {
-					Axes.X, Axes.Y, Axes.Z });
+		for (int i = 0; i < 3; i++) {
 
 			for(String networkSrc : networks) {
 				final Future<CommandModule> future = ij.command().run(GenericNetwork.class,
@@ -48,6 +48,7 @@ public class MultipleGenericNetworksTest extends CSBDeepTest {
 				assertEquals(1, result.size());
 				final Dataset output = result.get(0);
 				testResultAxesAndSize(input, output);
+				input = output;
 			}
 
 			System.out.println(i);
